23e696c85131eacf0dd5d3b729c32ad6b26002d2
IngaIbs <i-ibs@gmx.de>
2017-11-13 15:21:20 +0100

    experimental setup, changes in functional skript

--- ga/run.py
+++ ga/run.py
@@ -1,5 +1,7 @@
 from ga import *
 import imp
+import pickle
+from time import time
 
 # * other than random initalize
 # * fancy crossover, only have n-splits at the moment
@@ -30,20 +32,64 @@ def fitness(chromosome, job_times):
 	worst_time = np.max(time_per_machine)
 	return -worst_time
 
+def function_grid(param_grid):
+
+    gen_chromosome = lambda: gen_random_chromosome(len(jobs), num_machines)
+    alleles = list(range(num_machines))
+    count = 0
+    max_generations = 2000
+    stats_array = np.zeros((max_generations, 2, 8))
+    time_array = np.zeros((8,))
+    # change to two if new functions implemented
+    for i in range(1):
+        for j in range(2):
+            for k in range(1):
+                for l in range(2):
+                    for m in range(2):
+                        start = time()
+                        temp_stats = evolve(
+                                	fitness_func = partial(fitness, job_times = jobs),
+                                	init_func = partial(initalize, n = population_size, gen_chromosome = gen_chromosome,choice = param_grid[0][i]),
+                                	selection_func = partial(select, n = 25,choice = param_grid[1][i]),
+                                	crossover_func = partial(recombine, num_splits = 3, choice = param_grid[2][i]),
+                                	mutation_func = partial(mutate, p = 0.01, alleles=alleles, choice = param_grid[3][i]),
+                                	replacement_func = partial(replace, n = 5, choice = param_grid[4][i]),
+                                	max_generations = max_generations)
+                        time_array[count] = time()-start
+                        limit = np.sum(jobs) / num_machines
+                        stats_array[:, :, count] = temp_stats
+                        print('Combination {} done'.format(count))
+
+                        count = count + 1
+    with open('makespan_1.pickle','wb') as f:
+        pickle.dump(stats_array,f,pickle.HIGHEST_PROTOCOL)
+                            
+    with open('makespan_1_times.pickle','wb') as f:
+        pickle.dump(time_array,f,pickle.HIGHEST_PROTOCOL)
+            
+    
+
 # run things!
-gen_chromosome = lambda: gen_random_chromosome(len(jobs), num_machines)
-alleles = list(range(num_machines))
-
-evolve(
-	fitness_func = partial(fitness, job_times = jobs),
-	init_func = partial(initalize, n = population_size, gen_chromosome = gen_chromosome),
-	selection_func = partial(select_roulette, n = 25),
-	crossover_func = partial(recombine_crossover, num_splits = 3),
-	mutation_func = partial(mutate_each_random, p = 0.01, alleles=alleles),
-	replacement_func = partial(replace_keep_best, n = 5),
-	max_generations = 2000)
-
-limit = np.sum(jobs) / num_machines
-print("lower limit: {}".format(limit))
 
 
+param_grid = [['random','random'],['roulette','n_best'],
+              ['crossover','crossover'],['random','each_random'], 
+              ['all','keep_best']]
+
+print(fitness)
+function_grid(param_grid)
+#stats_array = evolve(
+#	fitness_func = partial(fitness, job_times = jobs),
+#	init_func = partial(initalize, n = population_size, gen_chromosome = gen_chromosome),
+#	selection_func = partial(select_roulette, n = 25),
+#	crossover_func = partial(recombine_crossover, num_splits = 3),
+#	mutation_func = partial(mutate_each_random, p = 0.01, alleles=alleles),
+#	replacement_func = partial(replace_keep_best, n = 5),
+#	max_generations = 2000)
+
+#with open('test1.pickle','wb') as f:
+#    pickle.dump(stats_array,f,pickle.HIGHEST_PROTOCOL)
+#
+#print("lower limit: {}".format(limit))
+#
+#
