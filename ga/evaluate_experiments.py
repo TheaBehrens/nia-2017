e935976cffd16d899ffc256d9234d0908168889a
TheaBehrens <tbehrens@uos.de>
2017-11-13 23:41:11 +0100

    some corrections, plots with titles

--- ga/evaluate_experiments.py
+++ ga/evaluate_experiments.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Nov 13 12:44:51 2017
-
 @author: tbehrens, iibs
 """
 
@@ -10,19 +9,57 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 
-with open('makespan_1.pickle','rb') as f:
+with open('makespan_2_results.pickle','rb') as f:
     stats = pickle.load(f)
 
-with open('makespan_1_times.pickle','rb') as f:
+with open('makespan_2_times.pickle','rb') as f:
     times = pickle.load(f)
     
-    
-print(stats.shape)
-print(times)
+param_grid = [['random','random'],['roulette','n_best'],
+              ['crossover','crossover'],['random','each_random'], 
+              ['all','keep_best']]
 
-for i in range(8):
+limit_min = np.min(stats) - 30             
+limit_max = np.max(stats) + 100
+
+cnt = 0
+for i in range(1):
+    for j in range(2): #2   
+        for k in range(1):
+            for l in range(2): #2
+                for m in range(2): #2
+                    plt.figure()
+                    plt.plot(stats[:,0,cnt], label='mean')
+                    plt.hold(True)
+                    #plt.plot(stats[:,1,cnt], linestyle='none', marker='.', label='best')
+                    plt.plot(stats[:,1,cnt], label='best')
+                    title_str = 'init: ' + param_grid[0][i] + ', '
+                    title_str += 'select: '  + param_grid[1][j] + ', '
+                    title_str += 'rec: '  + param_grid[2][k] + ',\n '
+                    title_str += 'mut: '  + param_grid[3][l] + ', '
+                    title_str += 'repl: '  + param_grid[4][m]
+                    plt.title(title_str)
+                    plt.ylim(limit_min, limit_max)
+                    plt.legend()
+                    save_name = str(cnt) + '.pdf'
+                    plt.savefig(save_name)
+                    cnt += 1
+                    plt.show()
+
+
+
+
+
+'''
+for i in range(8): # 8
     plt.figure()
-    plt.plot(stats[i,:,0])
+    plt.plot(stats[:,0,i], label='mean')
     plt.hold(True)
-    plt.plot(stats[i,:,1])
+    plt.plot(stats[:,1,i], linestyle='none', marker='.', label='best')
+    plt.legend()
     plt.show()
+    #print('begin mean: ', stats[0,0,i], ' best: ', stats[0,1,i])
+    #print('end mean: ', stats[-5:-1,0,i])
+    #print(' best: ', stats[-5:-1,1,i])
+    #print('------')
+    '''
--- ga/run.py
+++ ga/run.py
@@ -19,7 +19,7 @@ def load_data(filename):
         f.close()
         return data.jobs, data.num_machines
 
-jobs, num_machines = load_data("makespan_1.dzn")  # need to change here for each problem
+jobs, num_machines = load_data("makespan_2.dzn")  # need to change here for each problem
 
 # problem specific functions
 def gen_random_chromosome(num_jobs, num_machines):
@@ -37,9 +37,10 @@ def function_grid(param_grid):
     gen_chromosome = lambda: gen_random_chromosome(len(jobs), num_machines)
     alleles = list(range(num_machines))
     count = 0
+    num_combinations = 8
     max_generations = 2000
-    stats_array = np.zeros((max_generations, 2, 8))
-    time_array = np.zeros((8,))
+    stats_array = np.zeros((max_generations, 2, num_combinations))
+    time_array = np.zeros((num_combinations,))
     # change to two if new functions implemented
     for i in range(1):
         for j in range(2):
@@ -50,10 +51,10 @@ def function_grid(param_grid):
                         temp_stats = evolve(
                                 	fitness_func = partial(fitness, job_times = jobs),
                                 	init_func = partial(initalize, n = population_size, gen_chromosome = gen_chromosome,choice = param_grid[0][i]),
-                                	selection_func = partial(select, n = 25,choice = param_grid[1][i]),
-                                	crossover_func = partial(recombine, num_splits = 3, choice = param_grid[2][i]),
-                                	mutation_func = partial(mutate, p = 0.01, alleles=alleles, choice = param_grid[3][i]),
-                                	replacement_func = partial(replace, n = 5, choice = param_grid[4][i]),
+                                	selection_func = partial(select, n = 25,choice = param_grid[1][j]),
+                                	crossover_func = partial(recombine, num_splits = 3, choice = param_grid[2][k]),
+                                	mutation_func = partial(mutate, p = 0.01, alleles=alleles, choice = param_grid[3][l]),
+                                	replacement_func = partial(replace, n = 5, choice = param_grid[4][m]),
                                 	max_generations = max_generations)
                         time_array[count] = time()-start
                         limit = np.sum(jobs) / num_machines
@@ -61,10 +62,10 @@ def function_grid(param_grid):
                         print('Combination {} done'.format(count))
 
                         count = count + 1
-    with open('makespan_1.pickle','wb') as f:
+    with open('makespan_2_results.pickle','wb') as f:
         pickle.dump(stats_array,f,pickle.HIGHEST_PROTOCOL)
                             
-    with open('makespan_1_times.pickle','wb') as f:
+    with open('makespan_2_times.pickle','wb') as f:
         pickle.dump(time_array,f,pickle.HIGHEST_PROTOCOL)
             
     
@@ -76,7 +77,6 @@ param_grid = [['random','random'],['roulette','n_best'],
               ['crossover','crossover'],['random','each_random'], 
               ['all','keep_best']]
 
-print(fitness)
 function_grid(param_grid)
 #stats_array = evolve(
 #	fitness_func = partial(fitness, job_times = jobs),
